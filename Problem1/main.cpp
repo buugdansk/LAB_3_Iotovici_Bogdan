c958c371d22413e20a9ea9e8549484fae5e41a7a
buugdansk <bogai1002@gmail.com>
2025-11-21 21:20:01 +0200

    a

--- Problem1/main.cpp
+++ Problem1/main.cpp
@@ -1,68 +1,53 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 class Rectangle {
-private:
-    double length;
-    double width;
+    float width;
+    float height;
+
 public:
-    Rectangle(double len, double wid) {
-        length = len;
-        width = wid;
+    Rectangle(float w, float h) {
+        width = w;
+        height = h;
     }
-    double getArea() const {
-        return length * width;
+
+    float area() {
+        return width * height;
     }
-    double getPerimeter() const {
-        return 2 * (length + width);
-    }
-    double getLength() const {
-        return length;
-    }
-    double getWidth() const {
-        return width;
+
+    float perimeter() {
+        return 2 * (width + height);
     }
 };
 
-int main(){
-    double len1, wid1, len2, wid2;
+int main() {
+    float w1, h1, w2, h2;
+    cin >> w1 >> h1 >> w2 >> h2;
 
-    cout << "length rectangle 1: ";
-    cin >> len1;
-    cout << "width rectangle 1: ";
-    cin >> wid1;
-    
-    cout << "length rectangle 2: ";
-    cin >> len2;
-    cout << "width rectangle 2: ";
-    cin >> wid2;
+    Rectangle r1(w1, h1);
+    Rectangle r2(w2, h2);
 
-    Rectangle rect1(len1, wid1);
-    Rectangle rect2(len2, wid2);
+    cout << r1.perimeter() << endl;
+    cout << r1.area() << endl;
+    cout << r2.perimeter() << endl;
+    cout << r2.area() << endl;
 
-    double p1 = rect1.getPerimeter();
-    double a1 = rect1.getArea();
-    double p2 = rect2.getPerimeter();
-    double a2 = rect2.getArea();
-
-    cout << "area of rect1 is " << a1 << " and perimeter of rect 1 is " << p1 << endl;
-    cout << "area of rect2 is " << a2 << " and perimeter of rect 2 is " << p2 << endl;
-
-    if (p1 > p2) {
-        cout << "rect1 perimeter is larger." << endl;
-    } else if (p2 > p1) {
-        cout << "rect2 perimeter is larger." << endl;
+    if (r1.area() > r2.area()) {
+        cout << "area r1 larger" << endl;
+    } else if (r1.area() < r2.area()) {
+        cout << "area r2 larger" << endl;
     } else {
-        cout << "both rectangles have the same perimeter." << endl;
+        cout << "same area" << endl;
     }
-    if (a1 > a2) {
-        cout << "rect1 area is larger." << endl;
-    } else if (a2 > a1) {
-        cout << "rect2 area is larger." << endl;
+
+    if (r1.perimeter() > r2.perimeter()) {
+        cout << "perimeter r1 larger" << endl;
+    } else if (r1.perimeter() < r2.perimeter()) {
+        cout << "perimeter r2 larger" << endl;
     } else {
-        cout << "both rectangles have the same area." << endl;
+        cout << "same perimeter" << endl;
     }
+
     return 0;
 }
--- Problem5/main.cpp
+++ Problem5/main.cpp
@@ -1,42 +1,48 @@
 #include <iostream>
-#include "ComplexNumber.h"
 
 using namespace std;
 
+class ComplexNumber {
+    float real;
+    float imag;
+
+public:
+    ComplexNumber(float r, float i) {
+        real = r;
+        imag = i;
+    }
+
+    float getReal() {
+        return real;
+    }
+
+    float getImag() {
+        return imag;
+    }
+
+    ComplexNumber add(ComplexNumber other) {
+        return ComplexNumber(real + other.real, imag + other.imag);
+    }
+
+    ComplexNumber multiply(ComplexNumber other) {
+        float r = (real * other.real) - (imag * other.imag);
+        float i = (real * other.imag) + (imag * other.real);
+        return ComplexNumber(r, i);
+    }
+};
+
 int main() {
-    double r1, i1, r2, i2;
+    float r1, i1, r2, i2;
+    cin >> r1 >> i1 >> r2 >> i2;
 
-    cout << "1st real: ";
-    cin >> r1;
-    cout << "1st imaginary: ";
-    cin >> i1;
+    ComplexNumber c1(r1, i1);
+    ComplexNumber c2(r2, i2);
 
-    cout << "2nd real: ";
-    cin >> r2;
-    cout << "2nd imaginary: ";
-    cin >> i2;
+    ComplexNumber sum = c1.add(c2);
+    ComplexNumber product = c1.multiply(c2);
 
-    ComplexNumber num1(r1, i1);
-    ComplexNumber num2(r2, i2);
-
-    ComplexNumber sum = num1.add(num2);
-    ComplexNumber product = num1.multiply(num2);
-    
-    cout << "sum is: " << sum.getReal(); //for real part
-
-    if (sum.getImag() >= 0) {
-        cout << "+ " << sum.getImag() << "i" << endl;
-    } else {
-        cout << "- " << -sum.getImag() << "i" << endl;
-    } //for imaginary part
-    
-    cout << "product is: " << product.getReal(); //for real part
-
-    if (product.getImag() >= 0) {
-        cout << "+ " << product.getImag() << "i" << endl;
-    } else {
-        cout << "- " << -product.getImag() << "i" << endl;
-    } //for imaginary part
+    cout << "suma: " << sum.getReal() << " + " << sum.getImag() << "i" << endl;
+    cout << "produs: " << product.getReal() << " + " << product.getImag() << "i" << endl;
 
     return 0;
 }
